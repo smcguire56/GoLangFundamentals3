2b74f850f6d14b1d57756ec49593e4aa692c1d4f
smcguire56 <sean.mcguire56@gmail.com>
2017-11-30 21:34:20 +0000

    added reflections and I am

--- regex.go
+++ regex.go
@@ -2,52 +2,84 @@ package main
 
 import (
 	"fmt"
-    "math/rand"
+	"math/rand"
 	"regexp"
+	"strings"
+	"time"
 )
-
-var tests = 0
 
 func main() {
 
-	 questions := []string {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	questions := []string{
+		"People say I look like both my mother and father.",
+		"Father was a teacher.",
+		"I was my father’s favourite.",
+		"I’m looking forward to the weekend.",
+		"My grandfather was French!",
 		"I am happy.",
 		"I am not happy father with your responses.",
 		"I am not sure that you understand the effect that your questions are having on me.",
 		"I am not sure that you understand the effect that your questions are having on me.",
-		"I am not sure that you understand the effect that your questions are having on me.",
-		"I am not sure that you understand the effect that your questions are having on me.",
-		"I am not sure that you understand the father effect that your questions are having on me.",
-		"I am not sure that you understand the effect that your questions are having on me.",
-		"I am supposed to just take what you’re saying at face value?" } 
+		"I am supposed to just take what you’re saying at face value?"}
 
 	//reader := bufio.NewReader(os.Stdin)
 	//text, _ := reader.ReadString('\n')
 
+	for i := 0; i < len(questions); i++ {
+		fmt.Print("You: ")
+		var text = questions[i]
+		fmt.Println(questions[i])
 
-    for i := 0; i < len(questions); i++ {
-		fmt.Print("Ask Eliza: ")
-		var text = questions[tests]
-		fmt.Println(questions[tests])
-
-		fmt.Println(ElizaResponse(text, tests))
-		tests++
+		fmt.Print("Eliza: ")
+		fmt.Println(ElizaResponse(text))
 	}
 }
 
-func ElizaResponse(text string, tests int) string{
-	response := []string { 
-	"I’m not sure what you’re trying to say. Could you explain it to me?",
-	"How does that make you feel?",
-	"Why do you say that?"}
-	
-	rand.Seed(int64(tests))
+func ElizaResponse(text string) string {
+	response := []string{
+		"I’m not sure what you’re trying to say. Could you explain it to me?",
+		"How does that make you feel?",
+		"Why do you say that?"}
 
-    r, _ := regexp.Compile("father")
-
-	if r.MatchString(text) && tests < 3 {
+	if matched, _ := regexp.MatchString(`(?i).*\bfather\b.*`, text); matched {
 		return "Why don’t you tell me more about your father?"
-	} else {
-	return response[rand.Intn(len(response))]
 	}
+
+	if matched, _ := regexp.MatchString(`(?i)i(?:'|\sa)?m (.*)`, text); matched {
+		return "why are you like that?"
+	}
+
+	return response[rand.Intn(len(response))]
+}
+
+// adapted from https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
+
+func Reflect(input string) string {
+	// Split the input on word boundaries.
+	boundaries := regexp.MustCompile(`\b`)
+	tokens := boundaries.Split(input, -1)
+
+	// List the reflections.
+	reflections := [][]string{
+		{`I`, `you`},
+		{`me`, `you`},
+		{`you`, `me`},
+		{`my`, `your`},
+		{`your`, `my`},
+	}
+
+	// Loop through each token, reflecting it if there's a match.
+	for i, token := range tokens {
+		for _, reflection := range reflections {
+			if matched, _ := regexp.MatchString(reflection[0], token); matched {
+				tokens[i] = reflection[1]
+				break
+			}
+		}
+	}
+
+	// Put the tokens back together.
+	return strings.Join(tokens, ``)
 }
